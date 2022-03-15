757f5c194821f579d1723b78f6c8a2cbc536ac16
QBush <ignor-KuBush@yandex.ru>
2022-03-15 20:04:08 +1000

    Humans_and_people

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,7 +3,7 @@ package ru.netology
 
 fun main() {
 
-    val likesCount = 61
+    val likesCount = 121
     likesCountVariant(likesCount)
 
 }
@@ -12,7 +12,8 @@ fun main() {
 fun likesCountVariant(likesCount: Int) {
     if (likesCount == 0) print("Ваш лайк будет первым!")
     else if (likesCount == 1000 || likesCount % 1000 == 0) println("Понравилось $likesCount людей")
-    else if (likesCount == 1 || likesCount % 10 == 1) println("Понравилось $likesCount человеку")
+
+    else if (likesCount == 1 || (likesCount % 10 == 1 && likesCount !=11)) println("Понравилось $likesCount человеку")
     else println("Понравилось $likesCount людям")
 
 }
